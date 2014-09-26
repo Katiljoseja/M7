9439519b4499ef75b7b77d0065f2dcfa87567973
JoseJavier <JoseJavier@JoseJa>
2014-09-26 16:37:08 +0200

    Inicio

---
+++ src/m3/programación/UF1/M3Programación.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m3.programación.UF1;
+
+/**
+ *
+ * @author JoseJavier
+ */
+public class M3Programación {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+              int n1 = 4;
+              int n2 = 5;
+              
+              int resultado;
+              
+              resultado = n1 + n2;
+              
+            System.out.println("resultado:" + resultado);
+
+              
+    }
+    
+}
